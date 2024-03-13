ec19a7507b2c8c791b22705f359c324b276fe784
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-03-13 16:04:07 +0100

    Add Login Page

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -1,1 +1,53 @@
-export const LogIn = () => {}
+import { Link } from "react-router-dom"
+
+export const LogIn = () => {
+  return (
+    <main className='flex flex-col justify-center items-center min-h-screen'>
+      <div className='flex flex-col w-full items-center'>
+        <form className='flex flex-col items-start w-6/12 gap-4 font-bold'>
+          <h1 className='text-5xl mb-4'>Log in</h1>
+
+          <div className='flex w-full gap-4'>
+            <input
+              type='text'
+              name='firstname'
+              id='firstname'
+              placeholder='First name'
+              className='p-4 bg-[#D9D9D9] w-full rounded-lg text-lg'
+            />
+            <input
+              type='text'
+              name='lastname'
+              id='lastname'
+              placeholder='Last name'
+              className='p-4 bg-[#D9D9D9] w-full rounded-lg text-lg'
+            />
+          </div>
+          <input
+            type='email'
+            name='email'
+            id='email'
+            placeholder='Email address'
+            className='p-4 bg-[#D9D9D9] w-full rounded-lg text-lg'
+          />
+          <input
+            type='submit'
+            value='Continue'
+            className='p-4 bg-black text-white cursor-pointer w-full rounded-lg text-lg'
+          />
+          <div className='flex justify-between text-gray-200 cursor-pointer font-bold w-full'>
+            <p className='font-light'>
+              Forgot Password?
+              <Link to='/' className='font-bold cursor-pointer ml-1'>
+                Reset
+              </Link>
+            </p>
+            <Link to='/' className='cursor-pointer'>
+              Back to home
+            </Link>
+          </div>
+        </form>
+      </div>
+    </main>
+  )
+}
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -11,38 +11,38 @@ export const SignUp = () => {
             name='email'
             id='email'
             placeholder='Email address'
-            className='p-4 bg-[#D9D9D9] w-full rounded-lg'
+            className='p-4 bg-[#D9D9D9] w-full rounded-lg text-lg'
           />
           <input
             type='password'
             name='password'
             id='password'
             placeholder='Password'
-            className='p-4 bg-[#D9D9D9] w-full rounded-lg'
+            className='p-4 bg-[#D9D9D9] w-full rounded-lg text-lg'
           />
           <input
             type='password'
             name='password-create'
             id='password-create'
             placeholder='Create Password'
-            className='p-4 bg-[#D9D9D9] w-full rounded-lg'
+            className='p-4 bg-[#D9D9D9] w-full rounded-lg text-lg'
           />
           <input
             type='password'
             name='password-confirm'
             id='password-confirm'
             placeholder='Confirm Password'
-            className='p-4 bg-[#D9D9D9] w-full rounded-lg'
+            className='p-4 bg-[#D9D9D9] w-full rounded-lg text-lg'
           />
           <input
             type='submit'
             value='Create account'
-            className='p-4 bg-black text-white cursor-pointer w-full rounded-lg'
+            className='p-4 bg-black text-white cursor-pointer w-full rounded-lg text-lg'
           />
           <div className='flex justify-between text-gray-200 cursor-pointer font-bold w-full'>
             <p className='font-light'>
-              Already have an account?{" "}
-              <Link to='/login' className='font-bold cursor-pointer'>
+              Already have an account?
+              <Link to='/login' className='font-bold cursor-pointer ml-1'>
                 Log in
               </Link>
             </p>
